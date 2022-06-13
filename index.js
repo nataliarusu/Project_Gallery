40b3a80c5564b551b749281364cc7caf09001e1a
Natalia Rusu <nataliarusu.rn@gmail.com>
2022-06-13 19:29:56 +0100

    Add event listener to buttons

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-/*const projectBtns = document.querySelectorAll('.card-btn');
+const projectBtns = document.querySelectorAll('.card-btn');
 const showProjectHandler=(ev)=>{    
-    const descrEl = ev.target.nextElementSibling;
-    const btnValue = descrEl.classList.contains("hidden")?"Close description":"Read description";
+    const descriptionEl = ev.target.nextElementSibling;
+    const btnValue = descriptionEl.classList.contains("hidden")?"Close description":"Read description";
     ev.target.innerHTML =btnValue;
-    descrEl.classList.toggle('hidden');   
+    descriptionEl.classList.toggle('hidden');   
 }
 
 for(const btn of projectBtns){
    
     btn.addEventListener('click', showProjectHandler);
 }
-*/
+
+/*
 const ul = document.querySelector('.cards');
 const showHideDescriptionHandler = (ev) => {
   if (ev.target.classList.contains('card-btn')) {
     //prevent execution of code in 'if block', if event occurs on other ul elements
-    const descrEl = ev.target.nextElementSibling;
-    const btnValue = descrEl.classList.contains('hidden')
+    const descriptionEl = ev.target.nextElementSibling;
+    const btnValue = descriptionEl.classList.contains('hidden')
       ? 'Close description'
       : 'Read description';
     ev.target.innerHTML = btnValue;
     ev.target.classList.toggle('expanded');
-    descrEl.classList.toggle('hidden');
+    descriptionEl.classList.toggle('hidden');
   }
 };
 
 ul.addEventListener('click', showHideDescriptionHandler);
+*/

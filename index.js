612ea0ad9858525ceee37d970971f6d9f2a0e084
Natalia Rusu <nataliarusu.rn@gmail.com>
2022-06-13 19:33:08 +0100

    Add style to triggered button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const showProjectHandler=(ev)=>{
     const descriptionEl = ev.target.nextElementSibling;
     const btnValue = descriptionEl.classList.contains("hidden")?"Close description":"Read description";
     ev.target.innerHTML =btnValue;
+    ev.target.classList.toggle('expanded');
     descriptionEl.classList.toggle('hidden');   
 }
 
@@ -10,21 +11,3 @@ for(const btn of projectBtns){
    
     btn.addEventListener('click', showProjectHandler);
 }
-
-/*
-const ul = document.querySelector('.cards');
-const showHideDescriptionHandler = (ev) => {
-  if (ev.target.classList.contains('card-btn')) {
-    //prevent execution of code in 'if block', if event occurs on other ul elements
-    const descriptionEl = ev.target.nextElementSibling;
-    const btnValue = descriptionEl.classList.contains('hidden')
-      ? 'Close description'
-      : 'Read description';
-    ev.target.innerHTML = btnValue;
-    ev.target.classList.toggle('expanded');
-    descriptionEl.classList.toggle('hidden');
-  }
-};
-
-ul.addEventListener('click', showHideDescriptionHandler);
-*/
